7564bd0ddc087292fc36f89b377585c0216e3658
BjnNowak <bjn.nowak@gmail.com>
2023-06-08 09:08:56 +0200

    add examples to get_map()

--- R/get_map.R
+++ R/get_map.R
@@ -7,6 +7,10 @@
 #' @import sf
 #'
 #' @examples
+#' # Load map
+#' get_map()%>%
+#' ggplot()+
+#' geom_sf()
 #' @export
 get_map<-function(){
   hex<-read_sf('https://github.com/BjnNowak/frex_db/raw/main/map/hex_grid.gpkg')
